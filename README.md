4ab702a373018e9ef6db0c025f5afc798dfdf049
jaw3l <16852455+jaw3l@users.noreply.github.com>
2023-08-04 17:45:29 +0300

    added comment lines to week_01
    changed result variable in concatenate_strings function. it creates new string instead of inheriting from string1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Polkadot Substrate Development Bootcamp
+
+## [Week 01 - Hands On Task](./week_01/README.md)
+
+*Implement a basic program that uses ownership concepts*
--- week_01/src/main.rs
+++ week_01/src/main.rs
@@ -1,13 +1,26 @@
+// Concatenate Two Strings
 fn concatenate_strings (string1: &str, string2: &str) -> String {
-    let mut result = String::from(string1);
+    // Inside the concatenate_strings function, create a new String called result. 
+    let mut result = String::new();
+    
+    // Use the push_str() method to append the contents of the first input string slice, followed by the second input string slice.
+    result.push_str(string1);
     result.push_str(string2);
+    
+    // Return the result string from the function.
     return result
 }
 
 fn main() {
+    // Create two String variables, string1 and string2, and initialize them with appropriate values.
     let string1 = "Rise";
     let string2 = "In";
+
+    // Call the concatenate_strings function with references to string1 and string2 as arguments (using string slices). 
+    // Store the result in a new variable called concatenated_string.
     let concatenated_string = concatenate_strings(string1, string2);
+
+    // Print the concatenated_string variable to the console.
     println!("{}", concatenated_string);
 }
 
